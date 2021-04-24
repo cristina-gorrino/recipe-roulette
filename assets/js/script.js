ee3e7cd85f4bc510a350773c970bd2fa0b5c9f4c
cgorrino <cgorrino03@gmail.com>
2021-04-24 11:07:58 -0700

    Adding styling to history buttons so they are evenly spaced.

--- assets/js/script.js
+++ assets/js/script.js
@@ -121,7 +121,7 @@ function makeHistoryButtons() {
         for (var i = 0; i < storedHistoryInfo.length; i++){
             var historyButtonEl = document.createElement("button");
             historyButtonEl.setAttribute("type", "button")
-            historyButtonEl.setAttribute("class", "waves-effect waves-light btn-large");
+            historyButtonEl.setAttribute("class", "history-button waves-effect waves-light btn-large");
             historyButtonEl.textContent = storedHistoryInfo[i].queryTerm;
             historyContainerEl.appendChild(historyButtonEl);
         } 
